ee8cc081a355b025cd9ed2787210fa80ead77be0
Aditya Rao <adityarao2005@gmail.com>
2024-05-06 23:46:28 -0400

    Add project files.

---
+++ WebCraft/WebCraft.cpp
@@ -0,0 +1,12 @@
+﻿// WebCraft.cpp : Defines the entry point for the application.
+//
+
+#include "WebCraft.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
